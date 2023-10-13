43c006a11f19d1b9140653c99f29a2650ed44745
AsifMahsud <Asifmahsud543@gmail.com>
2023-10-13 12:04:53 -0400

    add emotion detection logic

---
+++ emotion_detection.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    PAYLOAD = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    try:
+        response = requests.post(URL, headers=HEADERS, json=PAYLOAD)
+
+        if response.status_code == 200:
+            data = response.json()
+
+            emotions = data['emotionPredictions'][0]['emotion']
+            dominant_emotion = max(emotions, key=emotions.get)
+
+            output = {
+                'anger': emotions['anger'],
+                'disgust': emotions['disgust'],
+                'fear': emotions['fear'],
+                'joy': emotions['joy'],
+                'sadness': emotions['sadness'],
+                'dominant_emotion': dominant_emotion
+            }
+
+            return output
+        else:
+            return f"Error: {response.status_code} - {response.text}"
+    except Exception as e:
+        return f"Exception occurred: {e}"
